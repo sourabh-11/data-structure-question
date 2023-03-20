5aaff965daa17b2de07fc456d1a15049ad4818fc
sourabh-11 <sourabhjangid2002@gmail.com>
2023-03-20 21:18:36 +0530

    pair with element sorting

---
+++ pair with diff elelment sorting.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+   
+   bool pairfound(int arr,n,pair)
+   {
+   	for(int i=0;i<n;i++)
+   	{
+   		
+	   }
+   }
+   
+   
+   
+   
+int main()
+{
+	   int arr[] ={2,10,20,50};
+	   int pair  = 60;
+	   int size  = sizeof(arr)/sizeof(arr[1]);
+	   pairfound(arr,size,pair);
+	   cout<<size;
+	return 0;
+}
